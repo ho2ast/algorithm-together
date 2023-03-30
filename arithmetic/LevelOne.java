9bc0cf64dd2fb8fe1c5ab50cd280c791c54d127f
HO2AST <lucid7@hotmail.co.kr>
2023-03-31 01:37:54 +0900

    bakjoon_2557

---
+++ arithmetic/LevelOne.java
@@ -0,0 +1,13 @@
+package arithmetic;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class LevelOne {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Hello World!");
+        bw.close();
+    }
+}
