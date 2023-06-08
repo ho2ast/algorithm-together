e0c2ddad3b0c27245d4563310c6e54be0bf23e5b
HO2AST <lucid7@hotmail.co.kr>
2023-06-08 14:18:16 +0900

    baekjoon_25305

---
+++ sort/Level3.java
@@ -0,0 +1,75 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * <p> 응시자 수와 상을 받는 사람의 수가 주어질 때 상을 받는 커트라인을 출력하세요.
+ * <p> 문제 바로가기: <a href="https://www.acmicpc.net/problem/25305"/>Baekjoon_25305 커트라인</a>
+ */
+public class Level3 {
+    /**
+     * <p> 성적순으로 정렬 후 인덱스 (상을 받는 인원 수 - 1)값을 출력한다.
+     * <p> 정렬은 시간 복잡도가 O(n²)인 세가지 정렬 방법(버블, 선택, 삽입)을 이용한다.
+     * <p> 점수는 내림차순으로 정렬해야한다.
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        insertionSort(arr);
+        System.out.println(arr[k - 1]);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j + 1] > arr[j]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minIndex] < arr[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(arr, minIndex, i);
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int target = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && target > arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = target;
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
