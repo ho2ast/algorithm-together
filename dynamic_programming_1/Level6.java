f76dff5c1e35dc1f063a04ed1ef4cd756b5542f1
HO2AST <lucid7@hotmail.co.kr>
2023-08-06 22:19:19 +0900

    backjoon_1149

--- dynamic_programming_1/Level6.java
+++ dynamic_programming_1/Level6.java
@@ -3,58 +3,66 @@ package dynamic_programming_1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /**
- * <p> 모든 집을 규칙에 따라 색칠하는 최소 비용을 구하여 출력하세.
- * <p> 문제 바로가기: <a href="https://www.acmicpc.net/problem/1149"/>Baekjoon_1149 RGB 거</a>
+ * <p> 모든 집을 규칙에 따라 색칠하는 최소 비용을 구하여 출력하세요.
+ * <p> 문제 바로가기: <a href="https://www.acmicpc.net/problem/1149"/>Baekjoon_1149 RGB 거리</a>
  */
 public class Level6 {
+  final static int RED = 0;
+  final static int GREED = 1;
+  final static int BLUE = 2;
+  static int[][] colorCost;
+  static int[][] DP;
   /**
    * <p>첫 번째로 집의 개수가 주어지고, 그 다음 줄 부터 각각의 집의 색에 따른 색칠 비용이 주어진다(순서는 빨강, 초록, 파랑).
-   * <p>집의 개수와 길이가 같은 메모이제이션용 배열을 만든다.
-   * <p>집의 개수 만큼 반복을 하며 색칠한다.
-   * <p>규칙에 맞게 색칠을 하며 해당 집이 어떤 색으로 색칠 하였는지 메모이제이션 배열에 저장(색상은 순서대로 주어지므로 1, 2, 3 중 하나로 로 저장)
-   * <p>첫 번째 집은 세 가지 색상 중 최소 비용인 색상으로 색칠 한다.
-   * <p>이 후 주어진 규칙에 따라 같은 색으로는 연속해서 색칠 할수 없기 때문에
-   * <p>이전 집에서 칠했던 색상을 제외한 후 남은 색상 중 최소 값으로 색칠.
+   * <p>주어진 색칠 비용을 저장하기 위한 배열을 만든다 int[집의개수][3].
+   * <p>주어진 모든 경우의 비용을 저장할 수 있는 배열 DP를 동일하게 만들어 준다. int[집의개수][3].
+   * <p>DP를 이용하여 해당 색상을 선택했을 때 선택할 수 있는 집들의 색상의 비용을 DP에 저장한다.
    */
   public static void main(String[] args) {
     try (
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     ) {
-      int cost = 0;
       int n = Integer.parseInt(br.readLine());
-      int[] coloredArr = new int[n];
+      colorCost = new int[n][3];
+      DP = new int[n][3];
+
 
       for (int i = 0; i < n; i++) {
-        String[] costArr = br.readLine().split(" ");
-        if (i != 0) {
-          costArr[coloredArr[i - 1] - 1] = "0";
-        }
-        int minCostIndex = getMinCostIndex(costArr);
-        cost += Integer.parseInt(costArr[minCostIndex]);
-        coloredArr[i] = minCostIndex + 1;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        colorCost[i][RED] = Integer.parseInt(st.nextToken());
+        colorCost[i][GREED] = Integer.parseInt(st.nextToken());
+        colorCost[i][BLUE] = Integer.parseInt(st.nextToken());
       }
 
-      System.out.println(cost);
+      // DP의 첫번째 값(집)은 각 색상비용의 첫번째 값으로 초기화
+      DP[0][RED] = colorCost[0][RED];
+      DP[0][GREED] = colorCost[0][GREED];
+      DP[0][BLUE] = colorCost[0][BLUE];
+
+
+      System.out.print(Math.min(Paint_cost(n - 1, RED), Math.min(Paint_cost(n - 1, GREED), Paint_cost(n - 1, BLUE))));
+      System.out.println(1);
 
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
-  static int getMinCostIndex(String[] costArr) {
-    int minCost = Integer.MAX_VALUE;
-    int minCostIndex = -1;
+  static int Paint_cost(int N, int color) {
 
-    for (int j = 0; j < costArr.length; j++) {
-      int currentCost = Integer.parseInt(costArr[j]);
-      if (currentCost != 0 && minCost > currentCost) {
-        minCost = currentCost;
-        minCostIndex = j;
+    if (DP[N][color] == 0) {
+      if (color == RED) {
+        DP[N][RED] = Math.min(Paint_cost(N - 1, GREED), Paint_cost(N - 1, BLUE)) + colorCost[N][RED];
+      } else if (color == GREED) {
+        DP[N][GREED] = Math.min(Paint_cost(N - 1, RED), Paint_cost(N - 1, BLUE)) + colorCost[N][GREED];
+      } else {
+        DP[N][BLUE] = Math.min(Paint_cost(N - 1, RED), Paint_cost(N - 1, GREED)) + colorCost[N][BLUE];
       }
     }
 
-    return minCostIndex;
+    return DP[N][color];
   }
 }
