132483b1c1d42a639aac53f9da9020db077307e5
HO2AST <lucid7@hotmail.co.kr>
2023-08-08 23:23:20 +0900

    leetcode_No9PalindromeNumber

--- leetcode/easy/No9PalindromeNumber.java
+++ leetcode/easy/No9PalindromeNumber.java
@@ -9,22 +9,27 @@ public class No9PalindromeNumber {
    * <p>주어진 정수를 문자열로 변환 후 해당 수를 거꾸로 만든 후 비교한다.
    * <p>거꾸로 변환 하는 방법은 {@link String#length()}이용하여 길이만큼 loop를 돌며
    * <p>맨 뒷자리부터 {@link StringBuilder}에 저장한 후 비교한다.
+   * <p>새로운 풀이
+   * <p>주어진 정수의 절반까지 loop를 돌며 첫번째 자리와 n자리, 두번째 자리와 n-1자리를 비교해간다.
+   * <p>중간에 다를 경우 즉시 false, loop가 완전히 끝나면 true를 반환한다.
    */
   public static void main(String[] args) {
     Solution solution = new Solution();
-    System.out.println(solution.isPalindrome(-121));
+    System.out.println(solution.isPalindrome(121));
   }
 }
 
 class Solution {
   public boolean isPalindrome(int x) {
-    StringBuilder palindrome = new StringBuilder();
     String strX = String.valueOf(x);
+    int length = strX.length();
 
-    for (int i = strX.length() - 1; i > -1; i--) {
-      palindrome.append(strX.charAt(i));
+    for (int i = 0; i < length / 2; i++) {
+      if (strX.charAt(i) != strX.charAt(length - 1 - i)) {
+        return false;
+      }
     }
 
-    return strX.contentEquals(palindrome);
+    return true;
   }
 }
